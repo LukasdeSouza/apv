21ac5d89a8f29bccfa0dfd29571ef127ce46e3c4
LukasdeSouza <lucas@bestdeal.com.br>
2024-06-07 15:48:03 -0300

    feature: adicionando analytics da vercel no root do projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "500", "600", "700"]  });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
+      <Analytics/>
     </html>
   );
 }
