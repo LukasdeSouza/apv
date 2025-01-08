9586f88930b5c65e00fb46d63f8fffeb2028c533
unknown <lucasdesouzasilva112@gmail.com>
2025-01-08 16:35:22 -0300

    correção de link

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="https://pay.kiwify.com.br/NG9sVjp"
+                  href="https://pay.kiwify.com.br/vvInEcS"
                   onClick={handleClickBuy}
                   className="rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-slate-200 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 min-w-36"
                 >
