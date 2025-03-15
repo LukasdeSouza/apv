7f20ca34e869c598dceb27caae72857fb0b8cb78
unknown <lucasdesouzasilva112@gmail.com>
2025-03-15 00:43:44 -0300

    more fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "500", "600", "700"]  });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <Toaster position="top-center" />
         </body>
       <Analytics/>
+      <SpeedInsights/>
     </html>
   );
 }
